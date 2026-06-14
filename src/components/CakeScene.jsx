351e3bf01e604b58145b9ae8dfbb857dac4e40ed
Kavyamishra1234578 <mkavya719@gmail.com>
2026-06-15 00:18:30 +0530

    Update CakeScene.jsx

--- src/components/CakeScene.jsx
+++ src/components/CakeScene.jsx
@@ -204,7 +204,7 @@ export default function CakeScene() {
           outlineWidth={0.03}
           outlineColor="#ff4d6d"
         >
-         SHERU SHARMA ✨
+         SHERU G ✨
         </Text>
       </Float>
       {/* FAIRY LIGHTS */}
